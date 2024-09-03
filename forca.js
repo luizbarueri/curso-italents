8c57b50f7d3946d653d99bd0a53ca92638640f07
luizbarueri <luizbarueri@gmail.com>
2024-09-03 19:00:33 -0300

    primeiro commit

---
+++ forca.js
@@ -0,0 +1,8 @@
+palavras = ['gato', 'cachorro', 'rato', 'tatu']
+
+function escolherPalavras() {
+    const  p_palavra = document.getElementById('palavra')
+    p_palavra.innerHTML = palavras[0]   
+}
+
+//escolherPalavras()
